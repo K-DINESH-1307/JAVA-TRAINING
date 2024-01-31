a241c44481cca432842e006cd80385351772da6f
K-DINESH-1307 <158204617+K-DINESH-1307@users.noreply.github.com>
2024-01-31 14:31:50 +0530

    Add files via upload

---
+++ Mjava/MathOperations/MathOperations.java
@@ -0,0 +1,13 @@
+package mjava;
+
+public class MathOperations {
+public static void main(String args[])
+{
+	int a=20,b=5;
+	System.out.println("Addition : "+(a+b));
+	System.out.println("Subtraction : "+(a-b));
+	System.out.println("Multiplication : "+(a*b));
+	System.out.println("Division : "+(a/b));
+	System.out.println("Modulus : "+(a%b));
+}
+}
---
+++ Mjava/RelationalOperators/RelationOperator.java
@@ -0,0 +1,33 @@
+package mjava;
+
+public class RelationOperator {
+public static void main(String args[])
+{
+	int a=5,b=20,c=5;
+	if(b>a)
+	{
+	System.out.println("B is greater than A");	
+	}
+	if(a<b)
+	{
+	System.out.println("A is Less than B");	
+	}
+	if(c==a)
+	{
+	System.out.println("C is Equal to A");	
+	}
+	if(c!=b)
+	{
+	System.out.println("C is Not Equal to B");	
+	}
+	if(b>=20)
+	{
+	System.out.println("B is greater than or Equat to 20");	
+	}
+	if(a<=5)
+	{
+	System.out.println("A is Less than or Equal to 5");	
+	}
+
+}
+}
