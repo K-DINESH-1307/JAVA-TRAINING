98910b66b9bf7c4ddd445ecb742b5239c046f56e
K-DINESH-1307 <158204617+K-DINESH-1307@users.noreply.github.com>
2024-01-31 15:36:43 +0530

    Add files via upload

---
+++ Mjava/BiwiseOperators/BitwiseOperator.java
@@ -0,0 +1,11 @@
+package mjava;
+
+public class BitwiseOperator {
+public static void main(String args[])
+{
+int a=5,b=3;
+System.out.println("AND : "+(a&b));
+System.out.println("OR : "+(a|b));
+System.out.println("XOR : "+(a^b));
+}
+}
---
+++ Mjava/Post&Pre_Inc&Dec/Post_Pre_Increment.java
@@ -0,0 +1,15 @@
+package mjava;
+
+public class Post_Pre_Increment {
+	public static void main(String args[])
+	{
+		int a=5,b=10;
+		System.out.println("Post Increment : "+(a++));
+		a=5;
+		System.out.println("Pre Increment : "+(++a));
+		System.out.println("Post Decrement : "+(b--));
+		b=10;
+		System.out.println("Pre Decrement : "+(--b));
+	}
+	}
+
